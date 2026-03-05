8c7c07cf4c2838e98982972e4d9b0bee8229ce0b
RafiZFSIB1C <rafizf.fauzan25@gmail.com>
2026-03-05 12:46:16 +0700

    Menambahkan input tebakan dari user

--- main.dart
+++ main.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
 void main() {
   print("Game Tebak Angka");
-  
+
+  stdout.write("Masukkan tebakan anda: ");
+  int tebakan = int.parse(stdin.readLineSync()!);
+
+  print("Tebakan anda: $tebakan");
 }
